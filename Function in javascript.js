119d2ecff6f6d21a5dab498e7909d7cb5365e434
ThierryRakotomanana <105288252+ThierryRakotomanana@users.noreply.github.com>
2022-05-20 12:59:15 +0300

    Update Function in javascript.js

--- Function in javascript.js
+++ Function in javascript.js
@@ -54,3 +54,63 @@ console.log(kids)
 after that we can see the object change
 kids become an object without the uncle property
 */
+
+
+/*
+
+une version plus amelirée du clonnage d'objet
+
+*/
+
+	function Clone(clone , Objet){
+		for(let key in Objet){
+			if(typeof Objet[key] == 'object'){
+				clone[key] = {}
+				Clone(clone[key], Objet[key])
+			} else{
+				clone[key] = Objet[key]
+			}
+		}
+		return clone
+	}
+
+
+	let Animal = {
+		name : 'Animal',
+		type : 'no type',
+		walk : function(){
+			return 'Yes i can walk'
+		},
+		First : {
+		    name : 'heyhey',
+		    another : 'Preperty',
+		    and : 'Once again',
+		    Objet : {
+		        name : 'not empty object',
+		        Deep : {
+		            name : 'Ok it is over'
+		        },
+		    
+		    salut : function(){
+		        console.log('salut ${name}')
+		    }
+		}}
+	}
+
+	let cat = new Clone({}, Animal)
+
+	First = {
+	    name : 'heyhey',
+	    another : 'Preperty',
+	    and : 'Once again',
+	    Objet : {
+	        name : 'not empty object',
+	        Deep : {
+	            name : 'Ok it is over'
+	        },
+	    
+	    salut : function(){
+	        console.log('salut ${name}')
+	    }
+	}
+
