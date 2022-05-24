d15f50de2a782f320e6945cc6b320ef2a5c9440a
ThierryRakotomanana <105288252+ThierryRakotomanana@users.noreply.github.com>
2022-05-24 10:51:29 +0300

    Update Function in javascript.js

--- Function in javascript.js
+++ Function in javascript.js
@@ -182,3 +182,13 @@ function ShowPrime(numb = 0){
 	}
     return true
 }
+/*
+Not ninja
+*/
+function Primary(n) {
+    if( (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0 ) && ( n != 2 && n != 3 && n != 5 && n != 7 )) 
+    {
+        return false
+    }
+    return true
+}
